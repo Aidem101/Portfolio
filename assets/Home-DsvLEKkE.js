c890b0b75c6b57a55d78ea8332b12efcb840a17b
Ivan <vania2003433616@gmail.com>
2025-06-27 03:27:17 +0700

    Updates

---
+++ assets/Home-DsvLEKkE.js
@@ -0,0 +1,6 @@
+import{c as e,j as s}from"./index-CNSVEVPV.js";/**
+ * @license lucide-react v0.523.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],a=e("user",c),t=()=>s.jsxs("section",{className:"home",children:[s.jsxs("h1",{className:"title",children:["Привет, я Иван — ",s.jsx("span",{className:"highlight",children:"frontend"}),"-разработчик"]}),s.jsx("div",{className:"avatar",children:s.jsx(a,{size:60,color:"#666"})})]});export{t as default};
