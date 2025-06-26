54523b4984c78e2b3b7f77aafa1eb60bf4f743d1
Ivan <vania2003433616@gmail.com>
2025-06-27 04:18:09 +0700

    Updates

--- assets/Home-D1OGf_2o.js
+++ assets/Home-D1OGf_2o.js
@@ -1,4 +1,4 @@
-import{c as e,j as s}from"./index-CNSVEVPV.js";/**
+import{c as e,j as s}from"./index-CGzDO-ma.js";/**
  * @license lucide-react v0.523.0 - ISC
  *
  * This source code is licensed under the ISC license.
